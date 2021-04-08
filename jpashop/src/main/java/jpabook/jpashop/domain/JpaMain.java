b767c18684eceaf0250146144f26f48be9c4c007
JEONG PYO YONG <sur2@naver.com>
2021-04-08 21:30:36 +0900

    다대일[N:1]

--- jpashop/src/main/java/jpabook/jpashop/domain/JpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/JpaMain.java
@@ -8,35 +8,15 @@ import java.util.List;
 
 public class JpaMain {
 
-    //private static EntityManagerFactory emf = null;
+    private static EntityManagerFactory emf = null;
 
     public static void main(String[] args) {
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpashop");
+        emf = Persistence.createEntityManagerFactory("jpashop");
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team);
-
-            MemberT member = new MemberT();
-            member.setUsername("member01");
-            member.setTeam(team);
-
-            em.persist(member);
-
-            //em.flush();
-            //em.clear();
-
-            MemberT findMemberT = em.find(MemberT.class, member.getId());
-            List<MemberT> memberTs = findMemberT.getTeam().getMembers();
-//            Team findTeam = findMemberT.getTeam();
-//            System.out.println("findTeam = " + findTeam.getName());
-            for (MemberT m : memberTs) {
-                System.out.println("m = " + m.getUsername());
-            }
 
             tx.commit();
 
