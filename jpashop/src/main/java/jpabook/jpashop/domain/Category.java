2949928c91ccb5f8ae13adcea2b0de1c2585721a
JEONG PYO YONG <sur2@naver.com>
2021-04-21 21:05:46 +0900

    다대다 연관관계 테스트 - 중간 테이블생성

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "PARENT_ID")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+    @ManyToMany
+    // 중간 테이블 생성(테이블 연관관계에서 N:M은 불가능 하기때문)
+    @JoinTable(name = "CATEGORY_ITEM",
+            joinColumns = @JoinColumn(name = "CATEGORY_ID"),
+            inverseJoinColumns = @JoinColumn(name = "ITEM_ID")
+    )
+    private List<Item> items = new ArrayList<>();
+}
